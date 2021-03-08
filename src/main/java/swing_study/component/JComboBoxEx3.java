1ba2c1774d4b20f306f6d1c75cc6c2ac34350e02
mskim <net94@naver.com>
2021-03-08 12:24:26 +0900

    콤보박스예

---
+++ src/main/java/swing_study/component/JComboBoxEx3.java
@@ -0,0 +1,25 @@
+package swing_study.component;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+@SuppressWarnings("serial")
+public class JComboBoxEx3 extends JFrame {
+
+	private JPanel contentPane;
+
+
+	public JComboBoxEx3() {
+		setTitle("JComboBox 실전 예제");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
