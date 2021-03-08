a06f8bcc7494d591489d6d61f6d770d64e4b6219
mskim <net94@naver.com>
2021-03-08 12:51:11 +0900

    클래스 완료

--- src/main/java/swing_study/panel/Department.java
+++ src/main/java/swing_study/panel/Department.java
@@ -4,7 +4,7 @@ public class Department {
 	private int deptNo;
 	private String deptName;
 	private int floor;
-	
+
 //	private List<Employee> emplist;
 
 	public Department() {
@@ -43,6 +43,28 @@ public class Department {
 
 	public void setFloor(int floor) {
 		this.floor = floor;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + deptNo;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Department other = (Department) obj;
+		if (deptNo != other.deptNo)
+			return false;
+		return true;
 	}
 
 	@Override
